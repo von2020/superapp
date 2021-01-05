1bc20f78b2d3baca8d0c00bcb5072fae00a5d05b
von2020 <orumwensey@gmail.com>
2021-01-05 15:22:13 +0100

    TelnetSuperApp Commit

--- baseUrl.js
+++ baseUrl.js
@@ -1,4 +1,4 @@
-const url = process.env.BaseUrl || 'http://173.212.213.205:8090' // this is for production and live
+const url = process.env.BaseUrl || 'http://173.212.208.45:8000' // this is for production and live
 // i dont understand why we have to set this as it is directly to the db so this should be able to be hardcoded?
 
 
